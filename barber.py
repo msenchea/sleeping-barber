3c285a3c5d5f3ead2867459c7a42caccaba0a776
xequals1 <marius.senchea2@mail.dcu.ie>
2020-04-02 20:21:43 +0100

    broken 3 lock implementation

--- barber.py
+++ barber.py
@@ -2,10 +2,13 @@ import sys
 import threading
 import time
 import random
+import queue
 
-BARBERS = 1 # set amount of barbers here
-CUSTOMERS = 20 # set amount of customers for the day here
+BARBERS = 3 # set amount of barbers here
+CUSTOMERS = 10 # set amount of customers for the day here
 SEATS = 5 # set amount of seats in the waiting room here
+
+wr_lock = threading.Lock()  # lock for accessing of the waiting room
 
 ARRIVAL_WAIT = 0.01
 
@@ -14,66 +17,97 @@ def wait():
 
 class Barber(threading.Thread):
 	condition = threading.Condition()
-	customers = []
 	should_stop = threading.Event() #for when all customers have been serviced
+
+	def __init__(self, ID):
+		super().__init__()
+		self.ID = ID
+
 
 	def run(self):
 		while True:
 			with self.condition:
-				if not self.customers:
-					print("barber sleeping")
+				wr_lock.acquire()
+				if waiting_room.qsize == 0:
+					wr_lock.release()
+					#print(f"barber {self.ID} sleeping")
 					self.condition.wait() #sleep and wait for customer
-				current_customer = self.customers[0]
-			current_customer.trim()
+				else:
+					wr_lock.release()
 
-			if self.should_stop.is_set():
-				break
+				if self.should_stop.is_set():
+					print(f"barber {self.ID} has signed out for the day")
+					return
 
-
+				wr_lock.acquire()
+				if waiting_room.qsize == 0:
+					current_customer = waiting_room.get()
+					wr_lock.release()
+					current_customer.trim(self.ID)
+				else:
+					wr_lock.release()
 
 class Customer(threading.Thread):
 	WAIT = 0.05
+
+	def __init__(self, ID):
+		super().__init__()
+		self.ID = ID
+
 	def wait(self):
 		time.sleep(self.WAIT * random.random())
 
-	def trim(self):  # Called from Barber thread
+	def trim(self, barber_ID):  # Called from Barber thread
 		# Get a haircut
-		print("customer is getting a haircut")
+		#print(f"barber {barber_ID} is cutting customer {self.ID}'s hair")
+		self.wait()
+		#print(f"customer {self.ID}'s hair is cut.'")
 		self.serviced.set()
-
 
 	def run(self):
 		self.serviced = threading.Event()
 		# Grab the barbers' attention, add ourselves to the customers,
-		barbers[0].condition.notify()
-		barbers[0].customers.append(self)
-		print("waiting to be serviced")
+
+		wr_lock.acquire()
+		if not(waiting_room.qsize() >= waiting_room.maxsize):
+			waiting_room.put(self)
+			wr_lock.release()
+			with Barber.condition:
+				Barber.condition.notify(1)
+			#print(f"customer {self.ID} took a seat in the waiting room")
+		else:
+			wr_lock.release()
+			print(f"waiting room is full, customer {self.ID} is leaving.")
+			return
+
 		while not self.serviced.is_set():
 			continue
 
-
-		# and wait to be serviced
+		print(f"customer {self.ID} is leaving")
 
 
 if __name__ == "__main__":
+
 	all_customers = []          # list of all customers for the day
-	barbers = []            # list of all barbers
+	waiting_room = queue.Queue(SEATS)
+
+	for i in range(BARBERS):
+		barber_thread = Barber(i)
+		barber_thread.start()
+		#print(f"barber {i} started")	# using fstrings as they are easier to read than formatting - must have python 3.6 or above installed however.
+
+	for i in range(CUSTOMERS):
+		wait()
+		customer = Customer(i)
+		all_customers.append(customer)
+		customer.start()
+		#print(f"customer {i} started")
+
+	for customer in all_customers:
+		customer.join()  # Wait for all customers to leave
+	# Grab the barbers' attention and tell them all that it's time to leave - using conditions I think
 
 	for b in range(BARBERS):
-		wait()
-		b = Barber()
-		barbers.append(b)
-		b.start()
-		print("barber started")
-
-	for c in range(CUSTOMERS):
-		wait()
-		c = Customer()
-		all_customers.append(c)
-		c.start()
-		print("customer started")
-
-	for c in all_customers:
-		c.join()  # Wait for all customers to leave
-	# Grab the barbers' attention and tell them all that it's time to leave - using events I think
-	barbers[0].should_stop.set()
+		Barber.should_stop.set()
+		with Barber.condition:
+			Barber.condition.notify(1)
